6bdf73e69d37cbdf1885f9488fbc7fc3843f04e0
Minhcadarnian <markbuicadarnian@gmail.com>
2025-06-10 21:51:44 +0700

    reformat with shebang

--- clean.bash
+++ clean.bash
@@ -1,2 +1,4 @@
-find . -type f -name "*.pyc" -delete && find . -type d -name "__pycache__" -exec rm -r {} + | cat
-
+#!/usr/bin/env bash
+# Remove Python bytecode and cache directories
+find . -type f -name "*.pyc" -delete
+find . -type d -name "__pycache__" -exec rm -rf {} +
